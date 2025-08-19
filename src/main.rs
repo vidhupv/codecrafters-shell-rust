c20122039f0a88e4d1ac33d6bea80993834bfead
Vidhu Panhavoor Vasudevan <vidhuvasudevan.2000@gmail.com>
2025-08-18 23:31:00 -0700

    first commit - uncommenting

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    Uncomment this block to pass the first stage
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
-    // Wait for user input
+    Wait for user input
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 }
