0f44125d722fb345ef347517f12211db31ade15a
Vidhu Panhavoor Vasudevan <vidhuvasudevan.2000@gmail.com>
2025-08-20 00:41:39 -0700

    exit commit refactor

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new(); 
     stdin.read_line(&mut input).unwrap();
-    if input == "exit 0\n" {
+
+    let line = input.trim_end();
+    if line == "exit 0" {
         break;
     }
 
